65c4dfbce2960f3e606a548b6e7b51ef5e2e416a
Mursuxb <mursuxb@gmail.com>
2024-02-26 12:05:53 +0200

    Updated with new methods

--- src/main/java/T1/FahrToCelc.java
+++ src/main/java/T1/FahrToCelc.java
@@ -4,4 +4,8 @@ public class FahrToCelc {
     public Double fahrToCelc(Double fahr) {
         return (fahr - 32) * 5 / 9;
     }
+
+    public static Double kelvintoCel(Double kel){
+        return (kel-273.15);
+    }
 }
--- src/test/java/FahrToCelcTest.java
+++ src/test/java/FahrToCelcTest.java
@@ -10,5 +10,8 @@ public class FahrToCelcTest {
         assertEquals(0.0, ftc.fahrToCelc(32.0), 0.0001);
         assertEquals(100.0, ftc.fahrToCelc(212.0), 0.0001);
         assertEquals(-40.0, ftc.fahrToCelc(-40.0), 0.0001);
+
+        assertEquals(0.0, ftc.kelvintoCel(273.15), 0.0001);
+        assertEquals(-273.15, ftc.kelvintoCel(0.0), 0.0001);
     }
 }
